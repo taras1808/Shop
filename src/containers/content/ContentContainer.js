dc08ce517a2a12c6bfd5c17ae33ae772198ae084
taras1808 <taras1808@gmail.com>
2020-11-03 00:17:31 +0100

    SHOP-2 Navigation, Product page
    - Route, Switch, Router, Link, useParams, useLocation
    - ProductContentContainer
    - changes NavBar, Product, ContentContainer

--- src/containers/content/ContentContainer.js
+++ src/containers/content/ContentContainer.js
@@ -1,19 +1,32 @@
-import './ContentContainer.css';
-import ProductsContainer from '../products/ProductsContainer.js';
-import NavBar from '../../components/navbar/NavBar';
-import FiltersContainer from '../filters/FiltersContainer.js';
-import ParamsContainer from '../params/ParamsContainer.js';
+import './ContentContainer.css'
+import ProductsContainer from '../products/ProductsContainer.js'
+import NavBar from '../../components/navbar/NavBar'
+import FiltersContainer from '../filters/FiltersContainer.js'
+import ParamsContainer from '../params/ParamsContainer.js'
+import ProductContentContainer from '../product/ProductContentContainer.js'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 
 function ContentContainer (props) {
 	return (
 		<div className="container">
-      		<NavBar />
-			<ParamsContainer />
-			<div className="flex">
-				<FiltersContainer />
-				<ProductsContainer search={props.search} />
-			</div>
+			<Router >
+				<Switch>
+					<NavBar />
+				</Switch>
+				<Switch>
+					<Route path="/:productId">
+						<ProductContentContainer />
+					</Route>
+					<Route path="/">
+						<ParamsContainer />
+						<div className="flex">
+							<FiltersContainer />
+							<ProductsContainer search={props.search} />
+						</div>
+					</Route>
+        		</Switch>
+			</Router>
 		</div>
 	)
 }
