fdfdf0da6679fff276c49d43978e3536f340a476
taras1808 <taras1808@gmail.com>
2020-11-15 21:42:03 +0100

    SHOP-9 selectedPriceRange in useEffect

--- src/containers/search/SearchContainer.js
+++ src/containers/search/SearchContainer.js
@@ -47,7 +47,7 @@ export default function SearchContainer () {
 				}
 			}, (error) => {})
 
-	}, [historySearch])
+	}, [historySearch, selectedPriceRange])
 
 	useEffect(() => {
 		if (!historyProducers) {
