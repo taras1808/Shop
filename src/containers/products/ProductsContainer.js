7efd7783435ad261a9aa9a41de00c3c8e2ae4692
taras1808 <taras1808@gmail.com>
2020-12-19 23:40:39 +0100

    SHOP-21 Products pagination navigation

--- src/containers/products/ProductsContainer.js
+++ src/containers/products/ProductsContainer.js
@@ -3,6 +3,7 @@ import Product from '../../components/product/Product.js'
 import TempProduct from '../../components/product/temp/TempProduct.js'
 import { useParams, useHistory } from'react-router-dom'
 
+const limit = 6
 
 function ProductsContainer ({ items, isLoaded, error }) {
 
@@ -22,11 +23,25 @@ function ProductsContainer ({ items, isLoaded, error }) {
 		content = items.results.map((item, index) => (<Product key={index} item={item} />))
 	}
 
-	const arr = []
+	let arr = [1]
 
-	for (let i = 1; i <= Math.ceil(items.total / 3.0); i++) {
-		arr.push(i)
+	let page = parseInt(parameters.get('page') ?? 0)
+
+	for (let i = 2; i <= Math.ceil(items.total / limit); i++) {
+		if ((i < 7 && page < 4) || (i > Math.ceil(items.total / limit) - 6 && page > Math.ceil(items.total / limit) - 5)) {
+			arr.push(i)
+		} else {
+			if (page - 1 <= i && page + 3 >= i) {
+				arr.push(i)
+			} else if (i === Math.ceil(items.total / limit))  {
+				arr.push(i)
+			}
+		}
 	}
+
+	if (page > 3) arr.splice(1, 1, "...")
+	if (page < (Math.ceil(items.total / limit) - 4)) arr.splice(arr.length - 2, 1, "...")
+
 
 	return (
 		<div id="products-container-block">
@@ -38,7 +53,7 @@ function ProductsContainer ({ items, isLoaded, error }) {
 					onClick={_ => {
 						if (parseInt(parameters.get('page') ?? 0) <= 0) return
 
-						parameters.set('page', (parseInt(parameters.get('page')) ?? 1) - 1)
+						parameters.set('page', (parseInt(parameters.get('page') ?? 1)) - 1)
 
 						let params = Array.from(parameters)
 							.filter(e => `${e[1]}`.length > 0)
@@ -53,9 +68,12 @@ function ProductsContainer ({ items, isLoaded, error }) {
 					{
 						arr.map((i, index) => (
 							<div key={index}
-								className="products-container-navigation-content-item"
+								className={`products-container-navigation-content-item  ${(parseInt(parameters.get('page') ?? 0) + 1) === i ? 'active' : ''}`}
 								onClick={_ => {
-									parameters.set('page', i - 1)
+									if (i === '...') 
+										parameters.set('page', arr[index + 1] > page ? arr[index - 1] : arr[index + 1] - 2)
+									else
+										parameters.set('page', i - 1)
 
 									let params = Array.from(parameters)
 										.filter(e => `${e[1]}`.length > 0)
@@ -69,7 +87,7 @@ function ProductsContainer ({ items, isLoaded, error }) {
 				</div>
 				<div className="products-container-navigation-arrow-block"
 					onClick={_ => {
-						if (parseInt(parameters.get('page') ?? 0) + 1 >= Math.ceil(items.total / 3.0)) return
+						if (parseInt(parameters.get('page') ?? 0) + 1 >= Math.ceil(items.total / limit)) return
 
 						parameters.set('page', (parseInt(parameters.get('page') ?? 0) + 1))
 
